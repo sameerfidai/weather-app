370cddb0941da257a8c5c451ce5ddfd1408effa6
Sameer Fidai <samfidai@Sameers-MBP.attlocal.net>
2021-05-21 14:35:10 -0400

    JavaFX setup

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,13 +1,42 @@
 package com.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import com.google.gson.*;
+import javafx.scene.layout.*;
+
+public class App extends Application {
+
+    VBox root;
+    Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // setup scene
+        root = new VBox();
+        scene = new Scene(root);
+
+        // setup stage
+        stage.setTitle("WeatherApp");
+        stage.setScene(scene);
+        stage.setMinHeight(600);
+        stage.setMinWidth(600);
+        stage.sizeToScene();
+        stage.show();
     }
 }
---
+++ src/main/java/com/app/Driver.java
@@ -0,0 +1,9 @@
+package com.app;
+
+import javafx.application.Application;
+
+public class Driver {
+    public static void main(String[] args) {
+        Application.launch(App.class);
+    }
+}
